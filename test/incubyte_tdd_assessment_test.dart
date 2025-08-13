7b8ae6c61cec765e0e4181acfc7bd093f2448477
abhijeet <abhijeetbundela26111997@gmail.com>
2025-08-13 11:50:50 +0530

    test: add single-number, comma seperated(two numbers and any amount of numbers) test

--- test/incubyte_tdd_assessment_test.dart
+++ test/incubyte_tdd_assessment_test.dart
@@ -7,4 +7,16 @@ void main() {
   test('empty string returns 0', () {
     expect(add(''), equals(0));
   });
+
+  test('single number returns that number', () {
+    expect(add('1'), equals(1));
+  });
+
+  test('two numbers comma separated', () {
+    expect(add('1,5'), equals(6));
+  });
+
+  test('multiple numbers', () {
+    expect(add('1,2,3,4'), equals(10));
+  });
 }
