718654105a9b53907b49b736f1583b4c5e514e8d
abhijeet <abhijeetbundela26111997@gmail.com>
2025-08-13 12:03:56 +0530

    feat: parse single number, comma separated(two numbers and any amount of numbers)

--- lib/incubyte_tdd_assessment.dart
+++ lib/incubyte_tdd_assessment.dart
@@ -1,5 +1,18 @@
 int add(String input) {
   var numbers = input.trim();
   if (numbers.isEmpty) return 0;
-  return 0;
+  var sum = 0;
+
+  List<String> parts = numbers.split(RegExp(r'[,\n]'));
+
+  List<int> nums = [];
+  for (String part in parts) {
+    if (part.trim().isNotEmpty) {
+      nums.add(int.parse(part.trim()));
+    }
+  }
+  for (int num in nums) {
+    sum += num;
+  }
+  return sum;
 }
