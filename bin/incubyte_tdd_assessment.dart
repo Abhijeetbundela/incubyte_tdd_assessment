0df82c6df07849907a5af1b35c49fb636bfad949
abhijeet <abhijeetbundela26111997@gmail.com>
2025-08-13 11:44:54 +0530

    Initial commit

---
+++ bin/incubyte_tdd_assessment.dart
@@ -0,0 +1,5 @@
+import 'package:incubyte_tdd_assessment/incubyte_tdd_assessment.dart';
+
+void main(List<String> arguments) {
+  print('Hello world: ${add()}!');
+}
---
+++ lib/incubyte_tdd_assessment.dart
@@ -0,0 +1,3 @@
+int add() {
+  return 6 * 7;
+}
